1d6e6f4c9a92671e66f20973496e3b0fbaf41c5d
khadada <lkhadada@gmail.com>
2022-04-06 18:25:57 +0100

    I imported necessary modules and classes

---
+++ photoEdit.py
@@ -0,0 +1,5 @@
+# Importing necessary Module and classes
+import sys
+from PyQt5.QtWidgets import (QApplication,QMainWindow,QStatusBar,QAction,QTextEdit,QToolBar,QDockWidget)
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon
